4f03a5a653f09d0dbc7028c36f506bfd13f9b938
Rowan d'Auria <56480877+escucheon1@users.noreply.github.com>
2023-05-20 11:03:00 +0100

    moved fetch functions into utils file

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,19 @@ import io from 'socket.io-client';
 import Login from './login';
 import ChatView from './ChatView';
 import ChatList from './ChatList';
+import utils from './utils';
+
+const {
+  postJSON,
+  fetchChats,
+  fetchMsgsByChatID,
+  login,
+  fetchMyUser,
+  fetchUsers,
+} = utils;
 
 const socket = io('http://localhost:3000', { autoConnect: false });
 socket.on('connect', () => console.log(socket.id));
-
-async function login(email, password) {
-  const bodyJSON = { email, password };
-  const res = await fetch('/login', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(bodyJSON),
-  });
-  const json = await res.json();
-  return json;
-}
-
-async function fetchChats() {
-  const res = await fetch('auth/chats');
-  const chats = await res.json();
-  return chats;
-}
-
-async function fetchUsers() {
-  const res = await fetch('auth/users');
-  const users = await res.json();
-  return users;
-}
-
-async function fetchMsgsByChatID(id) {
-  const res = await fetch(`auth/messages?chat_id=${id}`);
-  const chat = await res.json();
-  return chat;
-}
-
-async function fetchMyUser() {
-  const res = await fetch('auth/users/me');
-  return res;
-}
-
-async function postJSON(endpoint, JSONData) {
-  const res = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(JSONData),
-  });
-  const json = await res.json();
-  return json;
-}
 
 function LoadingMessage() {
   return (
